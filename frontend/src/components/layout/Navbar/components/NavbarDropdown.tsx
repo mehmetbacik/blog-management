12799c04c9630a5439e254993ca94209bbb95972
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2025-03-03 21:29:41 +0300

    fix: improve dropdown behavior and event handling
    - Implemented an event listener to detect clicks outside the dropdown.
    - Added a ref (`dropdownRef`) to track the dropdown element.
    - Ensured proper cleanup of event listeners to prevent memory leaks.
    - Improved UX by maintaining dropdown state consistency.
    
    This update enhances the dropdown interaction by ensuring it closes when clicking outside,
    resulting in a more intuitive and seamless user experience.

--- frontend/src/components/layout/Navbar/components/NavbarDropdown.tsx
+++ frontend/src/components/layout/Navbar/components/NavbarDropdown.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { FaUser, FaSignOutAlt, FaCogs } from "react-icons/fa";
 import Link from "next/link";
 
@@ -14,45 +14,73 @@ const NavbarDropdown: React.FC<NavbarDropdownProps> = ({
   isDropdownOpen,
   setDropdownOpen,
   onLogout,
-}) => (
-  <div
-    className={`navbar__dropdown ${isDropdownOpen ? "active" : ""}`}
-    onClick={() => setDropdownOpen(!isDropdownOpen)}
-  >
-    <div className="navbar__username">
-      <span className="navbar__icon">
-        <FaUser />
-      </span>
-      <span>{user.username}</span>
-    </div>
-    {isDropdownOpen && (
-      <div className={`navbar__dropdown-menu ${isDropdownOpen ? "open" : ""}`}>
-        {user.role === "admin" && (
-          <div
-            onClick={() => window.open("/admin", "_blank")}
-            className="navbar__dropdown-item"
-          >
-            <span className="navbar__icon">
-              <FaCogs />
-            </span>
-            <span>Admin Panel</span>
-          </div>
-        )}
-        <Link href="/profile" className="navbar__dropdown-item">
-          <span className="navbar__icon">
-            <FaUser />
-          </span>
-          <span>Profile</span>
-        </Link>
-        <div className="navbar__dropdown-item" onClick={onLogout}>
-          <span className="navbar__icon">
-            <FaSignOutAlt />
-          </span>
-          <span>Logout</span>
-        </div>
+}) => {
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setDropdownOpen(false);
+      }
+    };
+
+    if (isDropdownOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isDropdownOpen, setDropdownOpen]);
+
+  return (
+    <div
+      className={`navbar__dropdown ${isDropdownOpen ? "active" : ""}`}
+      ref={dropdownRef}
+    >
+      <div
+        className="navbar__username"
+        onClick={() => setDropdownOpen(!isDropdownOpen)}
+      >
+        <span className="navbar__icon">
+          <FaUser />
+        </span>
+        <span>{user.username}</span>
       </div>
-    )}
-  </div>
-);
+      {isDropdownOpen && (
+        <div className={`navbar__dropdown-menu open`}>
+          {user.role === "admin" && (
+            <div
+              onClick={() => window.open("/admin", "_blank")}
+              className="navbar__dropdown-item"
+            >
+              <span className="navbar__icon">
+                <FaCogs />
+              </span>
+              <span>Admin Panel</span>
+            </div>
+          )}
+          <Link href="/profile" className="navbar__dropdown-item">
+            <span className="navbar__icon">
+              <FaUser />
+            </span>
+            <span>Profile</span>
+          </Link>
+          <div className="navbar__dropdown-item" onClick={onLogout}>
+            <span className="navbar__icon">
+              <FaSignOutAlt />
+            </span>
+            <span>Logout</span>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default NavbarDropdown;
