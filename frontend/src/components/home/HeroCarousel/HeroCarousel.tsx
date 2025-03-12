5dd2d8888838752ada42efe44ab78d810abb0797
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2025-03-12 22:59:31 +0300

    feat: update structure and improve styling
    - Removed unnecessary background styles and applied object-fit: cover for improved image scaling.
    - Enhanced styling for a modern look.
    - Updated CSS to ensure consistent responsiveness and visual hierarchy.
    - Added new images to the project under public/carousel/ directory to support the updated structure.

--- frontend/src/components/home/HeroCarousel/HeroCarousel.tsx
+++ frontend/src/components/home/HeroCarousel/HeroCarousel.tsx
@@ -7,9 +7,19 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 
 const slides = [
-  { id: 1, title: "Discover the World"},
-  { id: 2, title: "Your Journey Begins"},
-  { id: 3, title: "Travel with Ease"},
+  { id: 1, title: "Discover the World", image: "/carousel/Carousel-1.jpg" },
+  {
+    id: 2,
+    title: "Your Journey Begins",
+    image: "/carousel/Carousel-2.jpg",
+  },
+  { id: 3, title: "Travel with Ease", image: "/carousel/Carousel-3.jpg" },
+  { id: 4, title: "Discover the World", image: "/carousel/Carousel-4.jpg" },
+  {
+    id: 5,
+    title: "Your Journey Begins",
+    image: "/carousel/Carousel-5.jpg",
+  },
 ];
 
 const HeroCarousel = () => {
@@ -26,9 +36,8 @@ const HeroCarousel = () => {
       >
         {slides.map((slide) => (
           <SwiperSlide key={slide.id}>
-            <div
-              className="slide"
-            >
+            <div className="slide">
+              <img src={slide.image} alt={slide.title} className="slideImage" />
               <h2 className="title">{slide.title}</h2>
             </div>
           </SwiperSlide>
